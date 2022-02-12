e245233da8b3de3a8ecda49225f4aaabd4686e3b
amirrezaask <raskarpour@gmail.com>
2022-02-12 06:40:48 +0330

    done

--- lib/blog_web/controllers/posts.ex
+++ lib/blog_web/controllers/posts.ex
@@ -3,19 +3,17 @@ defmodule BlogWeb.Controllers.Post do
   import Ecto.Query, only: [from: 2]
 
   def index(conn, %{}) do
-    conn |> send_resp(200, "hello world")
+    titles = Blog.Repo.all(from p in Blog.Schema.Post, select: p.title)
+    conn |> send_resp(200, titles)
   end
-
 
   def show(conn, %{"title" => title}) do
-    %Blog.Schema.Post{body: body} = Blog.Repo.one(from p in Blog.Schema.Post, where: p.title == ^title, select: [:body])
+    body = Blog.Repo.one(from p in Blog.Schema.Post, where: p.title == ^title, select: p.body)
     conn |> send_resp(200, body)
   end
-
 
   def add(conn, %{"title" => title, "body" => body}) do
     {:ok, _} = Blog.Repo.insert(%Blog.Schema.Post{title: title, body: body})
     conn |> put_status(201) |> json(%{title: title, body: body})
   end
-
 end
